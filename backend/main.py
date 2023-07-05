e123d6e2ea632bd930bc0a5534471d27886a770f
davidle2810 <anhlq28102002@gmail.com>
2023-07-05 17:55:21 +0700

    update structure

--- backend/main.py
+++ backend/main.py
@@ -46,8 +46,38 @@ class LoginInfo(BaseModel):
     userID: str = '123456'
     username: str = 'ltnhan'
     password: str = 'Nhan123@'
-    created_at: str = '1-1-2000'
 
+class UserInfo(BaseModel):
+    userID: str = '123456'
+    fname: str = 'Le'
+    lname: str = 'Nhan'
+    email: str = 'ltnhan@gmail.com'
+    phone: str = '0123456789'
+    dob: str = '01-01-2000'
+    country: str = 'Vietnam'
+    gender: bool = 0 #0 male, 1 female
+    role: bool = 0 #0 mentee, 1 mentor
+
+class MentorInfo(BaseModel):
+    mentorID: str = '123456'
+    language: str = 'English'
+    description: str = 'Hello'
+    rating: float = 1.5
+
+class MentorField(BaseModel):
+    mentorID: str = '123456'
+    fieldID: str = '123456'
+
+class Field(BaseModel):
+    fieldID: str = '123456'
+    fieldName: str = 'business'
+
+class Experience(BaseModel):
+    userID: str = '123456'
+    position: str = 'CEO'
+    workplace: str = 'Google'
+    startdate: str = '01-01-2023'
+    enddate: str = '01-01-2025'
 
 #               DATABASE COMMAND
 class Database:
